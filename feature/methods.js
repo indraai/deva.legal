8ccac692d4e4f91b6f39a97092387006c5f69dc5
Quinn <quinn@indra.ai>
2023-07-09 17:02:10 -0700

    update feature

---
+++ feature/methods.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /**************
+  method: Legal
+  params: packet
+  describe: The global service feature that installs with every agent
+  ***************/
+  legal(packet) {
+    this.context('feature');
+    const legal = this.legal();
+    const data = {};
+    return new Promise((resolve, reject) => {
+      this.question(`#docs raw feature/legal`).then(doc => {
+        data.doc = doc.a.data;
+        const info = [
+          `## Legal`,
+          `::begin:legal:${legal.id}`,
+          `client: ${legal.client_name}`,
+          `concerns: ${legal.concerns.join(', ')}`,
+          `::end:legal:${this.hash(legal)}`,
+        ].join('\n');
+        const text = doc.a.text.replace(/::info::/g, info)
+        return this.question(`#feecting parse ${text}`)
+      }).then(feecting => {
+        return resolve({
+          text: feecting.a.text,
+          html: feecting.a.html,
+          data: legal
+        });
+      }).catch(err => {
+        return this.error(err, packet, reject);
+      })
+    });
+  },
+};
