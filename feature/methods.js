96112c1f667cdbc141633bea46e0b868c08f8913
Quinn Michaels <quinn@indra.ai>
2025-08-31 13:53:22 -0700

    update Legal Layer

--- feature/methods.js
+++ feature/methods.js
@@ -1,55 +1,11 @@
 export default {
   /**************
-  method: Legal
+  method: legal
   params: packet
-  describe: The global service feature that installs with every agent
+  describe: The global legal feature that installs with every agent
   ***************/
-  legal(packet) {
-    this.context('feature');
-    return new Promise((resolve, reject) => {
-      const legal = this.legal();
-      const agent = this.agent();
-      const global = [];
-      legal.global.forEach((item,index) => {
-        global.push(`::begin:${item.key}:${item.id}`);
-        for (let x in item) {
-          global.push(`${x}: ${item[x]}`);
-        }
-        const thehash = this.lib.hash(item);
-        global.push(`hash: ${thehash}`);
-        global.push(`::end:${item.key}:${thehash}`);
-      });
-      const concerns = [];
-      legal.concerns.forEach((item, index) => {
-        concerns.push(`${index + 1}. ${item}`);
-      })
-      
-      const info = [
-        '::BEGIN:LEGAL',
-        `::begin:client`,
-        '## Client',
-        `id: ${legal.client_id}`,
-        `client: ${legal.client_name}`,
-        `::end:client}`,
-        concerns.length ? `::begin:concerns` : '',
-        concerns.length ? '## Concerns' : '',
-        concerns.length ? concerns.join('\n') : '',
-        concerns.length ? `::end:concerns` : '',
-        '::begin:global',
-        '## Global',
-        global.join('\n'),
-        '::end:global',
-        '::END:LEGAL',
-      ].join('\n');
-      this.question(`${this.askChr}feecting parse ${info}`).then(feecting => {
-        return resolve({
-          text: feecting.a.text,
-          html: feecting.a.html,
-          data: legal.concerns,
-        });
-      }).catch(err => {
-        return this.error(err, packet, reject);
-      })
-    });
+  async legal(packet) {
+    const legal = await this.methods.sign('legal', 'default', packet);
+    return legal;
   },
 };
